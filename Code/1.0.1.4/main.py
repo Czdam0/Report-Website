140eba1d261f1ad483ef9c5989c95d34d17e30db
Czdam <Czdam888@gmail.com>
2023-04-03 18:52:12 +0200

    1.0.1.4
    **+ guardio support**
    **+ reportphishing@apple.com support**
    **+ reportphishing@apwg.org support**
    **+ CISA support**
    **+ fbi support**

---
+++ Code/1.0.1.4/main.py
@@ -0,0 +1,147 @@
+import webbrowser, time, pymsgbox
+type = pymsgbox.confirm('Do you want to report this site?', 'Report', ["http", 'https', 'scan only domain', 'full scan'])
+
+print(type)
+
+def checkreason(reason):
+    if reason == None or "" or " ":
+        error()
+    else:
+        return reason
+    
+def checkurl(url):
+    if url == None or "" or " ":
+        error()
+    else:
+        return url
+
+def checknext(check):
+    if check == 'stop':
+        print("stop")
+        quit()
+    else:
+        return print("good")
+def error():
+    print("error") 
+    pymsgbox.alert('Report a Website had an error', 'error')
+    quit()
+
+def Report(url):
+
+    reason = checkreason(pymsgbox.prompt('Please submit a reason for reporting this website?'))
+    webbrowser.open_new_tab("https://phish.report/"+url)
+    checknext(pymsgbox.confirm('Continue?', 'continue?', ["continue", 'stop']))
+    webbrowser.open_new_tab("https://safebrowsing.google.com/safebrowsing/report_phish/?url=http://"+url+"/")
+    checknext(pymsgbox.confirm('Continue?', 'continue?', ["continue", 'stop']))
+    webbrowser.open_new_tab("https://www.microsoft.com/en-us/wdsi/support/report-unsafe-site")
+    checknext(pymsgbox.confirm('Continue?', 'continue?', ["continue", 'stop']))
+    webbrowser.open_new_tab("https://support.drweb.com/new/urlfilter")
+    checknext(pymsgbox.confirm('Continue?', 'continue?', ["continue", 'stop']))
+    webbrowser.open_new_tab("https://help.guard.io/hc/en-us/requests/new")
+    checknext(pymsgbox.confirm('Continue?', 'continue?', ["continue", 'stop']))
+    webbrowser.open_new_tab("https://www.fortiguard.com/webfilter")
+    checknext(pymsgbox.confirm('Continue?', 'continue?', ["continue", 'stop']))
+    webbrowser.open_new_tab("https://www.brightcloud.com/tools/url-ip-lookup.php")
+    checknext(pymsgbox.confirm('Continue?', 'continue?', ["continue", 'stop']))
+    webbrowser.open_new_tab("https://threatcenter.crdf.fr/submit_url.html")
+    checknext(pymsgbox.confirm('Continue?', 'continue?', ["continue", 'stop']))
+    webbrowser.open_new_tab("https://report.netcraft.com/report")
+    checknext(pymsgbox.confirm('Continue?', 'continue?', ["continue", 'stop']))
+    webbrowser.open_new_tab("https://urlfiltering.paloaltonetworks.com/")
+    checknext(pymsgbox.confirm('Continue?', 'continue?', ["continue", 'stop']))
+    webbrowser.open_new_tab("https://phishing.eset.com/en-us/report")
+    checknext(pymsgbox.confirm('Continue?', 'continue?', ["continue", 'stop']))
+    webbrowser.open_new_tab("https://scumware.org/add_url.php")
+    checknext(pymsgbox.confirm('Continue?', 'continue?', ["continue", 'stop']))
+    webbrowser.open_new_tab("https://www.avast.com/report-malicious-file.php")
+    checknext(pymsgbox.confirm('Continue?', 'continue?', ["continue", 'stop']))
+    webbrowser.open_new_tab("https://global.sitesafety.trendmicro.com/index.php")
+    checknext(pymsgbox.confirm('Continue?', 'continue?', ["continue", 'stop']))
+    webbrowser.open_new_tab("https://www.bitdefender.com/consumer/support/answer/29358/")
+    checknext(pymsgbox.confirm('Continue?', 'continue?', ["continue", 'stop']))
+    webbrowser.open_new_tab("https://sitelookup.mcafee.com/")
+    checknext(pymsgbox.confirm('Continue?', 'continue?', ["continue", 'stop']))
+    webbrowser.open_new_tab("https://csi.forcepoint.com/")
+    checknext(pymsgbox.confirm('Continue?', 'continue?', ["continue", 'stop']))
+    webbrowser.open_new_tab("https://sitereview.symantec.com/#/")
+    checknext(pymsgbox.confirm('Continue?', 'continue?', ["continue", 'stop']))
+    webbrowser.open_new_tab("https://www.spam404.com/report.html")
+    checknext(pymsgbox.confirm('Continue?', 'continue?', ["continue", 'stop']))
+    webbrowser.open_new_tab("https://www.avira.com/en/analysis/submit-url")
+    checknext(pymsgbox.confirm('Continue?', 'continue?', ["continue", 'stop']))
+    webbrowser.open_new_tab("https://talosintelligence.com/reputation_center")
+    checknext(pymsgbox.confirm('Continue?', 'continue?', ["continue", 'stop']))
+    webbrowser.open_new_tab("https://opentip.kaspersky.com/")
+    checknext(pymsgbox.confirm('Continue?', 'continue?', ["continue", 'stop']))
+    webbrowser.open_new_tab("https://reportfraud.ftc.gov/#/")
+    checknext(pymsgbox.confirm('Continue?', 'continue?', ["continue", 'stop']))
+    webbrowser.open_new_tab("https://www.cisa.gov/report")
+    checknext(pymsgbox.confirm('Continue?', 'continue?', ["continue", 'stop']))
+    webbrowser.open_new_tab("https://www.ic3.gov/Home/ComplaintChoice")
+    checknext(pymsgbox.confirm('Continue?', 'continue?', ["continue", 'stop']))
+    webbrowser.open_new_tab("mailto:reportphishing@apple.com?subject="+url+"&body="+url+"%20rwas%20repoted%20using%20an%20automated%20tool%20for%20the%20reason"+reason)
+    checknext(pymsgbox.confirm('Continue?', 'continue?', ["continue", 'stop']))
+    webbrowser.open_new_tab("mailto:reportphishing@apwg.org?subject="+url+"&body="+url+"%20rwas%20repoted%20using%20an%20automated%20tool%20for%20the%20reason"+reason)
+
+
+
+if type == "http" or "https" or "scan only domain" or "full scan":
+
+    if type == "http":
+        print("please copy url without dir like this www.example.com and no like this https://www.example.com/home")
+        url = checkurl(pymsgbox.prompt('url? (please copy url without dir like this www.example.com and no like this https://www.example.com/home)'))
+        print(url)
+        print("http scan")
+        webbrowser.open_new_tab("https://www.virustotal.com/gui/search/https%253A%252F%252F"+url)
+        print("Do you want to report this site?")
+        report = pymsgbox.confirm('Do you want to report this site?', 'Report', ["Yes", 'No'])
+        print(report)
+        if report == "Yes":
+            Report(url)
+        else:
+            pass
+    elif type == "https":
+        print("please copy url without dir like this www.example.com and no like this https://www.example.com/home")
+        url = checkurl(pymsgbox.prompt('url? (please copy url without dir like this www.example.com and no like this https://www.example.com/home)'))
+        print(url)
+        print("https scan")
+        webbrowser.open_new_tab("https://www.virustotal.com/gui/search/http%253A%252F%252F"+url)
+        print("Do you want to report this site?")
+        report = pymsgbox.confirm('Do you want to report this site?', 'Report', ["Yes", 'No'])
+        print(report)
+        if report == "Yes":
+            Report(url)
+        else:
+            pass
+    elif type == "scan only domain":
+        print("please copy url without dir like this www.example.com and no like this https://www.example.com/home")
+        url = checkurl(pymsgbox.prompt('url? (please copy url without dir like this www.example.com and no like this https://www.example.com/home)'))
+        print(url)
+        print("Domain scan")
+        webbrowser.open_new_tab("https://www.virustotal.com/gui/domain/"+url)
+        print("Do you want to report this site?")
+        report = pymsgbox.confirm('Do you want to report this site?', 'Report', ["Yes", 'No'])
+        print(report)
+        if report == "Yes":
+            Report(url)
+        else:
+            pass
+    elif type == "full scan":
+        print("please copy url without dir like this www.example.com and no like this https://www.example.com/home")
+        url = checkurl(pymsgbox.prompt('url? (please copy url without dir like this www.example.com and no like this https://www.example.com/home)'))
+        print(url)
+        print("full scan")
+        webbrowser.open_new_tab("https://www.virustotal.com/gui/domain/"+url)
+        webbrowser.open_new_tab("https://www.virustotal.com/gui/search/https%253A%252F%252F"+url)
+        webbrowser.open_new_tab("https://www.virustotal.com/gui/search/http%253A%252F%252F"+url)
+        print("Do you want to report this site?")
+        report = pymsgbox.confirm('Do you want to report this site?', 'Report', ["Yes", 'No'])
+        print(report)
+        if report == "Yes":
+            Report(url)
+        else:
+            pass
+    else:
+        error()
+else:
+    error()
